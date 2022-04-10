300d43fb9e87b6d20f872bdd7de6aea09edb1238
GreedNeSS <ruslan199126@gmail.com>
2022-04-10 17:58:16 +0300

    Добавьте файлы проекта.

---
+++ InitializeDB/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoredFunction.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public int CompanyId { get; set; }
+        public Company? Company{ get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Age: {Age}";
+        }
+    }
+}
---
+++ StoredFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿using StoredFunction;
+using StoredFunction.Models;
+
+Console.WriteLine("***** Stored Function *****");
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    db.Database.EnsureCreated();
+
+    Company microsoft = new Company { Name = "Microsoft" };
+    Company apple = new Company { Name = "Apple" };
+    User ruslan = new User { Name = "Ruslan", Age = 30, Company = microsoft };
+    User marcus = new User { Name = "Marcus", Age = 45, Company = microsoft };
+    User tod = new User { Name = "Tod", Age = 22, Company = apple };
+    User terry = new User { Name = "Terry", Age = 18, Company = apple };
+    
+    db.AddRange()
+}
