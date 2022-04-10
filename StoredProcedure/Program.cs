ec49dd482c1c92d7833ea4ec46c80c621bec3294
GreedNeSS <ruslan199126@gmail.com>
2022-04-10 19:25:40 +0300

    Added new procedure.

--- StoredProcedure/Program.cs
+++ StoredProcedure/Program.cs
@@ -17,3 +17,20 @@ using (ApplicationContext db = new ApplicationContext())
         Console.WriteLine(user);
     }
 }
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    Console.WriteLine("\ndb.Users.FromSqlRaw(\"GetUserWithMaxAge @userName OUT:\", param)\n");
+
+    SqlParameter param = new SqlParameter
+    {
+        ParameterName = "@userName",
+        SqlDbType = System.Data.SqlDbType.VarChar,
+        Direction = System.Data.ParameterDirection.Output,
+        Size = 50
+    };
+    
+    db.Database.ExecuteSqlRaw("GetUserWithMaxAge @userName OUT", param);
+
+    Console.WriteLine(param.Value);
+}
